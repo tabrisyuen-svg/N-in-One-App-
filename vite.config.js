0cd12bddb6effc79bf01ea6eb7852a3263c45a1e
tabrisyuen-svg <tabrisyuen@gmail.com>
2026-08-27 15:20:03 +0800

    Refactor Vite config to optimize xlsx dependency
    Removed Rollup options for external xlsx and added it to optimizeDeps.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['xlsx'],
-      output: {
-        globals: {
-          xlsx: 'XLSX'
-        }
-      }
-    }
+  optimizeDeps: {
+    include: ['xlsx']
   }
 })
